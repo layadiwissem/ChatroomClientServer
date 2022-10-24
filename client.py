927939c076e15ba4448fcfd3529196196df7b852
layadiwissem <80516364+layadiwissem@users.noreply.github.com>
2022-10-24 13:29:35 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,62 @@
+# Etudiants : Layadi et Missara
+
+# --------Script pour Tkinter GUI chat client----------
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+
+    #S'occupe de la reception des messages.
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg_list.insert(tkinter.END, msg)
+        except OSError:  # client peut avoir quitté le chat
+            break
+
+    # S'occupe de l'envoie des messages.
+def send(event=None):  # event est passé par bind
+    msg = my_msg.get()
+    my_msg.set("")  # Effacer input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{q}":
+        client_socket.close()
+        top.quit()
+
+    # Cette fonction est appellée quand la fenetre est fermée
+def on_closing(event=None):
+    my_msg.set("{q}")
+    send()
+
+top = tkinter.Tk()
+top.title("Chatroom multi-clients ")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # Pour le message à envoyer.
+my_msg.set("")
+scrollbar = tkinter.Scrollbar(messages_frame) 
+# Pour le container des messages (interface)
+msg_list = tkinter.Listbox(messages_frame, height=30, width=90, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Envoyer", command=send)
+send_button.pack()
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+#            -----------Partie sockets----------
+HOST = "127.0.0.1" 
+PORT = 9001
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop()  # Commencer  l'execution du GUI
---
+++ serveur.py
@@ -0,0 +1,58 @@
+# Etudiants : Layadi et Missara
+# ---------Serveur prend en charge plusieurs clients en même temps----------
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+# Prend en charge le clients entrants
+def accept_incoming_connections():
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s est connecté" % client_address)
+        client.send(bytes("Bienvenue dans le chatroom! Tapez votre nom et cliquez Envoyer", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+  # S'occupe d'une seule connexion client
+def handle_client(client):  # Prend la socket client en argument
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Bienvenue %s! Si vous voulez quitter, tapez {q} pour sortir.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s a rejoin le chat!" % name
+    Discussion(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            Discussion(msg, name + " : ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            Discussion(bytes("%s a quitté le chat." % name, "utf8"))
+            break
+
+ # Discussion pour envoyer un message à tous les clients
+def Discussion(msg, prefix=""):  # prefix pour identifier le nom
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+# -------------------------Partie sockets-------------------------
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 9001
+BUFSIZ = 1024
+
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind((HOST , PORT))
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
